5f34a7386b6aabb18e70bb55c25bc34a96dbf883
pron1x <lukas98z@web.de>
2024-06-27 23:05:41 +0200

    Implement PoC permissions check

--- subathon-bot/src/main/java/com/pronixxx/subathon/bot/SubathonBot.java
+++ subathon-bot/src/main/java/com/pronixxx/subathon/bot/SubathonBot.java
@@ -2,12 +2,19 @@ package com.pronixxx.subathon.bot;
 
 import com.github.twitch4j.TwitchClient;
 import com.github.twitch4j.chat.events.channel.ChannelMessageEvent;
+import com.github.twitch4j.common.enums.CommandPermission;
+import com.github.twitch4j.common.events.domain.EventUser;
 import com.pronixxx.subathon.bot.service.RabbitMessageService;
 import com.pronixxx.subathon.util.interfaces.HasLogger;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 @Component
 public class SubathonBot implements HasLogger {
@@ -18,6 +25,8 @@ public class SubathonBot implements HasLogger {
     @Value("${bot.subathon.command.prefix}")
     private String COMMAND_PREFIX;
 
+    private Map<BotCommand, List<CommandPermission>> permissionsMap;
+
     @Autowired
     RabbitMessageService messageService;
 
@@ -26,6 +35,9 @@ public class SubathonBot implements HasLogger {
 
     @PostConstruct
     public void init() {
+        permissionsMap = new HashMap<>();
+        permissionsMap.put(BotCommand.TIMER_START, List.of(CommandPermission.BROADCASTER, CommandPermission.MODERATOR));
+
         twitchClient.getChat().joinChannel(CHANNEL_NAME);
 
         twitchClient.getEventManager().onEvent(ChannelMessageEvent.class, event -> {
@@ -41,12 +53,27 @@ public class SubathonBot implements HasLogger {
         String[] args = event.getMessage().trim().split(" ");
         String command = args[0].substring(1);
         switch (command) {
+            case "timerstart" -> handleStartCommand(event.getPermissions(), event.getUser());
             case "timerpause" -> getLogger().info("Pausing timer");
             case "timerresume" -> getLogger().info("Resuming timer");
             case "timeradd" -> getLogger().info("Adding time");
             case "timerremove" -> getLogger().info("Removing time");
             default -> getLogger().info("Unknown command '{}'!", command);
         }
+    }
+
+    private void handleStartCommand(Set<CommandPermission> permissions, EventUser user) {
+        if(hasPermission(permissions, BotCommand.TIMER_START)) {
+            getLogger().info("Starting the timer!");
+        }
+    }
+
+    private boolean hasPermission(Set<CommandPermission> permissions, BotCommand command) {
+        return permissions.stream().anyMatch(permission -> permissionsMap.get(command).contains(permission));
+    }
+
+    enum BotCommand {
+        TIMER_START
     }
 
 }
