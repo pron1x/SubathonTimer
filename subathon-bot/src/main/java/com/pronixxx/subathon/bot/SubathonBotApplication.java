e8ef763ade68af3e5fea49a2954a692f3b9de610
pron1x <lukas98z@web.de>
2024-06-27 16:41:42 +0200

    Add subathon-bot module

---
+++ subathon-bot/src/main/java/com/pronixxx/subathon/bot/SubathonBotApplication.java
@@ -0,0 +1,11 @@
+package com.pronixxx.subathon.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubathonBotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SubathonBotApplication.class, args);
+    }
+}
