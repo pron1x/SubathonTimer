c62ee47e9dfe1124a6a9c85883d52c018f84bdb8
pron1x <lukas98z@web.de>
2024-06-27 22:42:29 +0200

    Implement first PoC for handling commands

--- subathon-bot/src/main/java/com/pronixxx/subathon/bot/SubathonBot.java
+++ subathon-bot/src/main/java/com/pronixxx/subathon/bot/SubathonBot.java
@@ -1,16 +1,22 @@
 package com.pronixxx.subathon.bot;
 
-import com.github.philippheuer.events4j.api.domain.IDisposable;
 import com.github.twitch4j.TwitchClient;
 import com.github.twitch4j.chat.events.channel.ChannelMessageEvent;
 import com.pronixxx.subathon.bot.service.RabbitMessageService;
 import com.pronixxx.subathon.util.interfaces.HasLogger;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SubathonBot implements HasLogger {
+
+    @Value("${bot.subathon.channel}")
+    private String CHANNEL_NAME;
+
+    @Value("${bot.subathon.command.prefix}")
+    private String COMMAND_PREFIX;
 
     @Autowired
     RabbitMessageService messageService;
@@ -20,9 +26,27 @@ public class SubathonBot implements HasLogger {
 
     @PostConstruct
     public void init() {
-        twitchClient.getChat().joinChannel("pronixxx");
-        IDisposable handlerReg = twitchClient.getEventManager().onEvent(ChannelMessageEvent.class, event -> {
-            getLogger().info("Received message event! [{}]{}:{}", event.getPermissions().toString(), event.getUser().getName(), event.getMessage());
+        twitchClient.getChat().joinChannel(CHANNEL_NAME);
+
+        twitchClient.getEventManager().onEvent(ChannelMessageEvent.class, event -> {
+            getLogger().trace("Received message. [{}: {}]", event.getUser().getName(), event.getMessage());
+            if(event.getMessage().startsWith(COMMAND_PREFIX)) {
+                handleCommand(event);
+            }
         });
     }
+
+    private void handleCommand(ChannelMessageEvent event) {
+        getLogger().debug("Received command [{}: {}]", event.getUser().getName(), event.getMessage());
+        String[] args = event.getMessage().trim().split(" ");
+        String command = args[0].substring(1);
+        switch (command) {
+            case "timerpause" -> getLogger().info("Pausing timer");
+            case "timerresume" -> getLogger().info("Resuming timer");
+            case "timeradd" -> getLogger().info("Adding time");
+            case "timerremove" -> getLogger().info("Removing time");
+            default -> getLogger().info("Unknown command '{}'!", command);
+        }
+    }
+
 }
---
+++ subathon-bot/src/main/java/com/pronixxx/subathon/bot/service/RabbitMessageService.java
@@ -0,0 +1,16 @@
+package com.pronixxx.subathon.bot.service;
+
+import com.pronixxx.subathon.bot.config.RabbitMQConfig;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMessageService {
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    public void sendMessage(String message) {
+        rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE_NAME, RabbitMQConfig.ROUTING_KEY, message);
+    }
+}
