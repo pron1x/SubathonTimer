823a8eb5ac932149e8c46cedc4ac1312bf5be65b
pron1x <lukas98z@web.de>
2024-07-10 22:22:46 +0200

    Implement ignoring mock events

--- subathon-dataservice/src/main/java/com/pronixxx/subathon/MessageReceiver.java
+++ subathon-dataservice/src/main/java/com/pronixxx/subathon/MessageReceiver.java
@@ -11,6 +11,7 @@ import com.pronixxx.subathon.datamodel.enums.SubTier;
 import com.pronixxx.subathon.service.TimerService;
 import com.pronixxx.subathon.util.interfaces.HasLogger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
@@ -18,6 +19,8 @@ import java.util.Map;
 
 @Component
 public class MessageReceiver implements HasLogger {
+    @Value("${timer.ignoremock}")
+    private boolean ignoreMock;
     @Autowired
     ObjectMapper objectMapper;
 
@@ -35,8 +38,9 @@ public class MessageReceiver implements HasLogger {
             throw new RuntimeException(e);
         }
 
-        if (event.isMock()) {
+        if (event.isMock() && ignoreMock) {
             getLogger().debug("Event is mock: {}", event);
+            return;
         }
 
         if (event.getType() == EventType.COMMAND) { // We handle bot commands differently
