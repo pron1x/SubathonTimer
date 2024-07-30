9a1bfc20f37d2b3981dcd60b7dd765d7cbdc42d0
pron1x <lukas98z@web.de>
2024-07-30 21:26:22 +0200

    Add green glow to timer when time is added

--- subathon-ui/src/main/java/com/pronixxx/subathon/ui/SubathonUiApplication.java
+++ subathon-ui/src/main/java/com/pronixxx/subathon/ui/SubathonUiApplication.java
@@ -2,11 +2,14 @@ package com.pronixxx.subathon.ui;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 @Push
+@Theme("subathon")
 public class SubathonUiApplication implements AppShellConfigurator {
 
     public static void main(String[] args) {
--- subathon-ui/src/main/java/com/pronixxx/subathon/ui/view/timer/TimerView.java
+++ subathon-ui/src/main/java/com/pronixxx/subathon/ui/view/timer/TimerView.java
@@ -6,11 +6,14 @@ import com.pronixxx.subathon.ui.service.TimerEventService;
 import com.pronixxx.subathon.ui.service.TimerEventService.TimerEventListener;
 import com.pronixxx.subathon.util.interfaces.HasLogger;
 import com.vaadin.flow.component.AttachEvent;
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.DetachEvent;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.concurrent.CompletableFuture;
 
 @Route(value = "timer")
 public class TimerView extends HorizontalLayout implements TimerEventListener, HasLogger {
@@ -18,6 +21,8 @@ public class TimerView extends HorizontalLayout implements TimerEventListener, H
     private final TimerEventService timerEventService;
 
     private final SubathonTimer timer;
+
+    private CompletableFuture<Void> stage;
 
     @Override
     protected void onAttach(AttachEvent attachEvent) {
@@ -46,8 +51,35 @@ public class TimerView extends HorizontalLayout implements TimerEventListener, H
                 ui -> ui.access(() -> {
                     getLogger().info("Handling TimerEvent: {}", timerEvent);
                     timer.updateWithNewEvent(timerEvent);
+                    addTimeAddedTheme();
                 })
         );
 
+    }
+
+    private void changeClassName(Component component, String className, boolean remove) {
+        getUI().ifPresent(ui -> ui.access(() -> {
+            if(remove) {
+                component.removeClassName(className);
+            } else {
+                component.addClassName(className);
+            }
+        }));
+    }
+
+    private void addTimeAddedTheme() {
+        if(stage != null && !stage.isDone()) {
+            stage.cancel(true);
+        } else {
+            changeClassName(timer, "neon", false);
+        }
+        stage = CompletableFuture.runAsync(() -> {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        stage.thenRun(() -> changeClassName(timer, "neon", true));
     }
 }
