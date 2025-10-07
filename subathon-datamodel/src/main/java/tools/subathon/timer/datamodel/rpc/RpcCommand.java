b01ca25379293b8fc74dab50f874c8c465479396
pron1x <online.accounts.lz+github@proton.me>
2025-10-07 09:42:51 +0200

    Remove old unused rpc classes

--- subathon-datamodel/src/main/java/tools/subathon/timer/datamodel/rpc/RpcCommand.java
+++
@@ -1,7 +0,0 @@
-package tools.subathon.timer.datamodel.rpc;
-
-public enum RpcCommand {
-    GET,
-    CREATE_OR_UPDATE,
-    DELETE
-}
--- subathon-datamodel/src/main/java/tools/subathon/timer/datamodel/rpc/RpcStatus.java
+++
@@ -1,7 +0,0 @@
-package tools.subathon.timer.datamodel.rpc;
-
-public enum RpcStatus {
-    OK,
-    NOT_FOUND,
-    ERROR
-}
