b003df4cf4a6fbbb3c624d65e8840ffb36d82d98
pron1x <online.accounts.lz+github@proton.me>
2026-01-26 21:23:11 +0100

    Remove deprecated VaadinWebSecurity in favour of VaadinSecurityConfigurer

--- subathon-ui/src/main/java/tools/subathon/timer/ui/config/SecurityConfiguration.java
+++ subathon-ui/src/main/java/tools/subathon/timer/ui/config/SecurityConfiguration.java
@@ -1,18 +1,21 @@
 package tools.subathon.timer.ui.config;
 
-import com.vaadin.flow.spring.security.VaadinWebSecurity;
+import com.vaadin.flow.spring.security.VaadinSecurityConfigurer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
-public class SecurityConfiguration extends VaadinWebSecurity {
+public class SecurityConfiguration {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.oauth2Login(customizer ->
-                customizer.loginPage("/oauth2/authorization/twitch").permitAll());
-        super.configure(http);
+    @Bean
+    protected SecurityFilterChain configureFilterChain(HttpSecurity http) throws Exception {
+        http.with(VaadinSecurityConfigurer.vaadin(), configurer -> {
+            configurer.oauth2LoginPage("/oauth2/authorization/twitch");
+        });
+        return http.build();
     }
 }
