7418ef4cfa0081b8f614a4318649c1c3020b0c66
pron1x <online.accounts.lz+github@proton.me>
2026-05-14 22:09:33 +0200

    Switch from deprecated theme to stylesheets

--- subathon-ui/src/main/java/tools/subathon/timer/ui/SubathonUiApplication.java
+++ subathon-ui/src/main/java/tools/subathon/timer/ui/SubathonUiApplication.java
@@ -1,8 +1,8 @@
 package tools.subathon.timer.ui;
 
+import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
-import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,7 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @Push
-@Theme(value = "subathon", variant = Lumo.DARK)
+@StyleSheet(Lumo.STYLESHEET)
+@StyleSheet("styles.css")
 public class SubathonUiApplication implements AppShellConfigurator {
 
     public static void main(String[] args) {
--- subathon-ui/src/main/java/tools/subathon/timer/ui/config/SecurityConfiguration.java
+++ subathon-ui/src/main/java/tools/subathon/timer/ui/config/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package tools.subathon.timer.ui.config;
 
 import com.vaadin.flow.spring.security.VaadinSecurityConfigurer;
+import org.springframework.boot.security.autoconfigure.web.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,10 +13,15 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration {
 
     @Bean
-    protected SecurityFilterChain configureFilterChain(HttpSecurity http) throws Exception {
+    protected SecurityFilterChain configureFilterChain(HttpSecurity http) {
         http.with(VaadinSecurityConfigurer.vaadin(), configurer -> {
             configurer.oauth2LoginPage("/oauth2/authorization/twitch");
         });
+
+        http.authorizeHttpRequests(auth -> {
+            auth.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll();
+        });
+
         return http.build();
     }
 }
